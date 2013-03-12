f19f1228366236924e868a25f9a7809135ad7608
jfwood <john.wood@rackspace.com>
2013-03-12 16:59:02 -0500

    Prepare for next release

--- keep/version.py
+++ keep/version.py
@@ -17,5 +17,5 @@
 Cloud Keep version
 """
 
-__version__ = '0.1.17'
-__version_info__ = tuple(__version__.split('0.1.17'))
+__version__ = '0.1.18dev'
+__version_info__ = tuple(__version__.split('0.1.18dev'))
