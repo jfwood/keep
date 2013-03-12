5a91d56c07571f3bef7e39dccfe2a759b53c36c9
jfwood <john.wood@rackspace.com>
2013-03-12 16:03:41 -0500

    Release for v0.1.6

--- keep/version.py
+++ keep/version.py
@@ -17,5 +17,5 @@
 Cloud Keep version
 """
 
-__version__ = '0.1.6dev'
-__version_info__ = tuple(__version__.split('0.1.6dev'))
+__version__ = '0.1.6'
+__version_info__ = tuple(__version__.split('0.1.6'))
