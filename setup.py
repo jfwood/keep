b48bd185b6348b7754261d4494912e49add32029
jfwood <john.wood@rackspace.com>
2013-03-20 16:29:30 -0700

    fixes to build config files

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# Copyright (c) 2010 OpenStack, LLC.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 try:
     from setuptools import setup, find_packages
+    from setuptools.command.sdist import sdist
 except ImportError:
     from ez_setup import use_setuptools
     use_setuptools()
     from setuptools import setup, find_packages
+    from setuptools.command.sdist import sdist
 
 # Determine version of this application.
 # TBD: Revisit version flows and processing once integrating with OpenStack, see glance setup.py
@@ -24,8 +41,8 @@ except EnvironmentError:
 class local_sdist(sdist):
     """Customized sdist hook - builds the ChangeLog file from VC first"""
 
-        def run(self):
-            sdist.run(self)
+    def run(self):
+        sdist.run(self)
 
 cmdclass = {'sdist': local_sdist}
 
